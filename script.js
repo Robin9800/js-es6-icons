65a68299b908cabed6eb023cdff069739a5ad503
Robin9800 <robin.costanzo@gmail.com>
2022-03-24 15:56:55 +0100

    Template created

--- script.js
+++ script.js
@@ -127,28 +127,32 @@ Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni ic
 il nome dell'icona e l'icona stessa.*/
 
 //Dichiaro una variabile collegata al container nell'html.
-const container = document.getElementById('container');
+const iconContainer = document.getElementById('body .containerIcone');
 
 //Creiamo una funzione che prenderà come parametro l'array di icone.
 createIcons(arrayIcone);
 
-function createIcons (data){
+function createIcons(data){
 
-    //Creo un ciclo forEach che mi crea ogni elemento sulla base del tipo standard inserito.
-    data.array.forEach(element => {
-    //<i class="fa-solid fa-cat"></i>
+    //Creo una variabile d'appoggio inzialmente vuota che verrà riempita una volta terminato il ciclo.
+    let allIcons = '';
+    //Creo un ciclo forEach che mi crea ogni elemento sulla base della struttura standard inserita.
+    data.forEach(element => {
 
-    //Destrutturo il file estraendo i dati che mi servono per realizzare il template.
-    const {family, prefix, name} = element
-    
-    //Creo la struttura standard del singolo elemento.
-    const template = `
-    <div>
-        <i class="${family} ${prefix} ${name}"></i>
-        <p>${name}</p>
+        //Destrutturo il file estraendo i dati che mi servono per realizzare il template.
+        const {family, prefix, name} = element
 
-    </div>
-    `
+        //Creo la struttura standard del singolo elemento.
+        const template = `
+        <div>
+            <i class="${family} ${prefix}${name}"></i>
+            <p>${name}</p>
+
+        </div>
+        `
+        //Una volta completato il ciclo aggiungo il template ad allIcons.
+        allIcons += template;
     });
-    
+    // Una volta finito di ciclare aggiungo la variabile contenente le icone dentro il container di icone.
+    iconContainer.innerHTML = allIcons;
 }
